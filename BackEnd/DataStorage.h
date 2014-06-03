89558677c027217c4a55e5018118713ac883e1f5
Elepole <blackhole.vs.darkhole@gmail.com>
2014-06-02 21:24:19 -0400

    Just some precision

--- BackEnd/DataStorage.h
+++ BackEnd/DataStorage.h
@@ -6,10 +6,10 @@
 class dataStorage {
 
 public:
-    virtual void storeData() = 0;
+    virtual void storeData() = 0;   //In an ideal world: call checkData, if then store the data.
 
 protected:
-    virtual void checkData() = 0;
+    virtual void checkData() = 0; //Call checkUrl and checkKeyWords. May also check other data if needed
     virtual void checkUrl() = 0;
     virtual void checkKeyWords() = 0;
 
