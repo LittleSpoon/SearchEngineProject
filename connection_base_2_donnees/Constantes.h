e44ccce9b0c157acfd731c1bb0d358cdbb860caf
FranckReynaud <160365@supinfo.com>
2014-06-21 15:11:05 -0400

    suppressing useless directory

--- connection_base_2_donnees/Constantes.h
+++
@@ -1,9 +0,0 @@
-#ifndef CONSTANTES_H
-#define CONSTANTES_H
-
-        const char* HOSTNAME = "localhost";
-        const char* USERNAME = "root";
-        const char* PASSWORD = "";
-        const char* DB_NAME = "projet";
-
-#endif // CONSTANTES_H
